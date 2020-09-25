bb028b449dd1bf395755e6487930657feb815783
qstion05 <qstion05@gmail.com>
2020-09-26 05:25:33 +0900

    목표 주석 작업 완료, 09/26

--- Brute_Force_Attack.py
+++ Brute_Force_Attack.py
@@ -5,6 +5,7 @@ Brute_Force_Attack 프로젝트.
 모르는 내용은 다같이 회의해봅시다!
 
 '''
+import zipfile  #zip파일 읽기 및 압축 해제 기능이 포함된 모듈.
 
 def zipfile_cheak():
 	#1번 파트, 해독할 파일 경로 찾기 및 zip파일 판정하기.
@@ -17,6 +18,12 @@ def make_string():
 def string_input():
 	#3번 파트, 생성 된 문자열 입력 및 T/F판정
 	#최지혁, 윤성우
+	'''
+	make_string에서 만든 문자열을  solve_key 변수에 저장.
+	solve_key 변수의 값을 zipfile_check에서 입력받은 경로의 zip파일 압축해제시 필요한 pwd 값에 대입.
+	틀리면 다음 문자열로 변경, 맞으면 그 문자열 리턴.
+	'''
+	solve_key = make_string()
 
 
 
